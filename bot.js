94077ef7e0692ef88137fb26f2fb6c5e5d22a041
Dood <41833445+icebotggMC@users.noreply.github.com>
2019-05-09 00:54:26 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,32 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("575801325904855060")
+setInterval(function() {
+channel.send(`you`);
+}, 30)
+})
+
+client.on('message', message => {
+        var prefix = "+";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'say')) {
+            message.channel.send(''+x);
+                message.delete(01000)
+        }
+        
+       
+      });
+
+client.login(process.env.BOT_TOKEN);
